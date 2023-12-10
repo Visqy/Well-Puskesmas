947736f82185b46e2c1f2c778855568cf2d110c1
Paneva <152346631+P4neva@users.noreply.github.com>
2023-12-10 17:37:40 +0700

    Update PemeriksaanAwal.cpp

--- PemeriksaanAwal.cpp
+++ PemeriksaanAwal.cpp
@@ -2,12 +2,17 @@
 #include<iomanip>
 using namespace std;
 
-struct PemeriksaanAwal {
-    int beratbadan, tinggibadan, suhubadan, tensidarah;
+struct tensiDarah{
+    float sistolik, diastolik;
+};
+
+struct pemeriksaanAwal {
+    float beratbadan, tinggibadan, suhubadan;
+    tensiDarah tds;
 };
 
 struct node {
-    PemeriksaanAwal data;
+    pemeriksaanAwal data;
     node* next;
 };
 
@@ -16,10 +21,11 @@ typedef node* pointernode;
 void createElementPemeriksaan(pointernode& pBaru) {
     pBaru = new node;
     cin.ignore();
-    cout << "Masukkan Berat Badan   : "; cin >> pBaru->data.beratbadan;
-    cout << "Masukkan Tinggi Badan  : "; cin >> pBaru->data.tinggibadan;
-    cout << "Masukkan Suhu Badan    : "; cin >> pBaru->data.suhubadan;
-    cout << "Masukkan Tensi Darah   : "; cin >> pBaru->data.tensidarah;
+    cout << "Masukkan Berat Badan       : "; cin >> pBaru->data.beratbadan;
+    cout << "Masukkan Tinggi Badan      : "; cin >> pBaru->data.tinggibadan;
+    cout << "Masukkan Suhu Badan        : "; cin >> pBaru->data.suhubadan;
+    cout << "Masukkan Nilai Sistolik    : "; cin >> pBaru->data.tds.sistolik;
+    cout << "Masukkan Nilai Diastolik   : "; cin >> pBaru->data.tds.diastolik;
     cout << endl;
     pBaru->next = NULL;
 }
@@ -27,19 +33,24 @@ void createElementPemeriksaan(pointernode& pBaru) {
 void displayOutputPemeriksaan(pointernode& first) {
     pointernode pBantu = first;
     cout << "Data Pemeriksaan Awal" << endl;
-    cout << setfill('-') << setw(60) << "" << setfill(' ') << endl;
-    cout << setw(15) << left << "Berat Badan"
-         << setw(15) << "Tinggi Badan"
-         << setw(15) << "Suhu Badan"
-         << setw(15) << "Tensi Darah" << endl;
-    cout << setfill('-') << setw(60) << "" << setfill(' ') << endl;
+    cout << setfill('-') << setw(80) << "" << setfill(' ') << endl;
+    cout << setw(15) << left << "|Berat Badan"
+         << setw(15) << "|Tinggi Badan"
+         << setw(15) << "|Suhu Badan"
+         << setw(15) << "|Tensi Darah" << endl;
+    cout << setfill('-') << setw(80) << "" << setfill(' ') << endl;
+    cout << setw(15) << "|" << setw(15) << "|" << setw(15) << "|"
+         << setw(15) << "|Sistolik"
+         << setw(15) << "|Diastolik" << endl;
+    cout << setfill('-') << setw(80) << "" << setfill(' ') << endl;
 
     while (pBantu != NULL) {
-        cout << setw(15) << pBantu->data.beratbadan << setw(15) << pBantu->data.tinggibadan
-             << setw(15) << pBantu->data.suhubadan << setw(15) << pBantu->data.tensidarah << endl;
+        cout << "|" << setw(14) << pBantu->data.beratbadan << "|" << setw(14) << pBantu->data.tinggibadan
+             << "|" << setw(14) << pBantu->data.suhubadan << "|" << setw(14) << pBantu->data.tds.sistolik
+             << "|" << setw(14) << pBantu->data.tds.diastolik << endl;
         pBantu = pBantu->next;
     }
-    cout << setfill('-') << setw(60) << "" << setfill(' ') << endl;
+    cout << setfill('-') << setw(80) << "" << setfill(' ') << endl;
 }
 
 int main() {
